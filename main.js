39ae2a48ebb61ff7f10035c0e826272a3e123829
Jay Orten <jo288@byu.edu>
2022-05-04 17:31:50 -0600

    update http

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ const app = Vue.createApp({
 
 	methods: {
 		refreshColors() {
-			var url = "http://colormind.io/api/";
+			var url = "colormind.io/api/";
 			var data = {
 				model : "default",
 				input : ["N","N","N","N","N"]
